4d66a0a8cc8aee5c3461d91661a4b1c664e47ab5
Pavan Sai.K <pk4981@srmist.edu.in>
2022-02-11 09:20:08 +0530

    22nd Problem

---
+++ Recursion/Kth_Permutation_Sequence.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/permutation-sequence/
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact=1;
+        vector<int> numbers;
+        int i;
+        for(i=1;i<n;i++)
+        {
+            fact*=i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(i);
+        k=k-1;
+        string ans="";
+        while(true)
+        {
+            ans = ans+to_string(numbers[k/fact]);
+            numbers.erase(numbers.begin()+k/fact);
+            if(numbers.size()==0)
+                break;
+            k=k%fact;
+            fact=fact/numbers.size();
+        }
+        return ans;
+    }
+};
